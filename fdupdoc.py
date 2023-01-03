25a7fe5b9964c77b84a77df536727e6c424ed488
bushnerd <scutxd@gmail.com>
2023-01-03 16:30:16 +0800

    print to log

--- fdupdoc.py
+++ fdupdoc.py
@@ -12,8 +12,7 @@ import sys
 from docx import Document
 
 formatter = logging.Formatter(
-    '%(asctime)s - %(levelname)s - %(filename)s:%(lineno)d - %(funcName)s - %(message)s'
-)
+    '%(asctime)s-%(levelname)s-%(filename)s:%(lineno)d-%(message)s')
 LOG_FILE_PATH = os.path.dirname(__file__) + '/log/'
 LOG_FILE_NAME = 'log.log'
 LOG_FILE = LOG_FILE_PATH + LOG_FILE_NAME
@@ -22,7 +21,7 @@ console_handler = logging.StreamHandler()  # 输出到控制台的handler
 console_handler.setFormatter(formatter)
 console_handler.setLevel(logging.DEBUG)  # 设置控制台日志级别为ERROR
 
-file_handler = logging.FileHandler(LOG_FILE)  # 输出到文件的handler
+file_handler = logging.FileHandler(LOG_FILE, mode='w')  # 输出到文件的handler
 file_handler.setFormatter(formatter)
 file_handler.setLevel(logging.DEBUG)  # 设置文件日志级别为DEBUG
 
@@ -52,8 +51,8 @@ def msplit(s, seperators=',|\.|\?|，|。|？|！'):
 
 
 def readDocx(docfile):
-    print('*' * 80)
-    print('文件', docfile, '加载中……')
+    logger.info('*' * 80)
+    logger.info('文件{}加载中……'.format(docfile))
     t1 = datetime.datetime.now()
     paras = getText(docfile)
     segs = []
@@ -65,7 +64,7 @@ def readDocx(docfile):
         if len(temp) > 0:
             segs.append(temp)
     t2 = datetime.datetime.now()
-    print('加载完成，用时: ', t2 - t1)
+    logger.info('加载完成，用时: {}'.format(t2 - t1))
     showInfo(segs, docfile)
     return segs
 
@@ -77,9 +76,9 @@ def showInfo(doc, filename='filename'):
         for s in p:
             segs = segs + 1
             chars = chars + len(s)
-    print('段落数: {0:>8d} 个。'.format(len(doc)))
-    print('短句数: {0:>8d} 句。'.format(segs))
-    print('字符数: {0:>8d} 个。'.format(chars))
+    logger.info('段落数: {0:>8d} 个。'.format(len(doc)))
+    logger.info('短句数: {0:>8d} 句。'.format(segs))
+    logger.info('字符数: {0:>8d} 个。'.format(chars))
 
 
 def compareParagraph(doc1, i, doc2, j, min_segment=5):
@@ -112,28 +111,28 @@ def compareParagraph(doc1, i, doc2, j, min_segment=5):
     count = sum([len(s) for s in list])
     ratio = float(count) / min(len1, len2)
     if count > 10 and ratio > 0.1:
-        print(' 发现相同内容 '.center(80, '*'))
-        print('文件1第{0:0>4d}段内容：{1}'.format(i + 1, p1))
-        print('文件2第{0:0>4d}段内容：{1}'.format(j + 1, p2))
-        print('相同内容：', list)
-        print('相同字符比：{1:.2f}%\n相同字符数： {0}\n'.format(count, ratio * 100))
+        logger.info(' 发现相同内容 '.center(80, '*'))
+        logger.info('文件1第{0:0>4d}段内容：{1}'.format(i + 1, p1))
+        logger.info('文件2第{0:0>4d}段内容：{1}'.format(j + 1, p2))
+        logger.info('相同内容：{}'.format(list))
+        logger.info('相同字符比：{1:.2f}%\n相同字符数： {0}\n'.format(count, ratio * 100))
     return list
 
 
 if len(sys.argv) < 3:
-    print("参数小于2.")
+    logger.error("参数小于2.")
 
 doc1 = readDocx(sys.argv[1])
 doc2 = readDocx(sys.argv[2])
 
-print('开始比对...'.center(80, '*'))
+logger.info('开始比对...'.center(80, '*'))
 
 t1 = datetime.datetime.now()
 for i in range(len(doc1)):
     if i % 100 == 0:
-        print('处理进行中，已处理段落 {0:>4d} (总数 {1:0>4d} ） '.format(i, len(doc1)))
+        logger.info('处理进行中，已处理段落 {0:>4d} (总数 {1:0>4d} ） '.format(i, len(doc1)))
     for j in range(len(doc2)):
         compareParagraph(doc1, i, doc2, j)
 
 t2 = datetime.datetime.now()
-print('\n比对完成，总用时: ', t2 - t1)
+logger.info('比对完成，总用时: {}'.format(t2 - t1))
